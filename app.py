a86e2f5c565e13bb1f518b549f2b99ef32b1a741
CorruptedPhysco <106505409+CorruptedPhysco@users.noreply.github.com>
2025-06-28 19:08:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -20,4 +21,4 @@ def get_link():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
