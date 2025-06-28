d55ca335b6e69ef62fc41bc3bc14857cebf143f9
CorruptedPhysco <deepu.342008@gmail.com>
2025-06-28 19:01:58 +0530

    Initial upload with app.py and requirements.txt

---
+++ app.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+def get_first():
+    url="https://bingotingo.com/best-social-media-platforms/"
+    response = requests.get(url)
+    soup=BeautifulSoup(response.text,"html.parser")
+    div= soup.find("a",class_="su-button su-button-style-soft su-button-wide")
+    print(div["href"])
+    return(div["href"])
+
+ 
+@app.route("/get-link", methods=["GET"])
+def get_link():
+    result = get_first()
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
